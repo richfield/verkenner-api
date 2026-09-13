0688fde0f1f6d63025edd496ec4d02cc80eda9f8
Richard ten Velde <richard@ten-velde.com>
2026-09-13 21:44:27 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const Constants = {
     VerkennersSpreadSheetId: "1_OIZYeh0pL7leC0GmmRkj-W6O_q6tRjlMthBl8oxf2U",
-    OpkomstSheetName: "Planning",
-    OpkomstRange: "'Planning'!A1:H1000",
+    OpkomstSheetName: "Opkomsten",
+    OpkomstRange: "'Opkomsten'!A1:H1000",
     LeidingRange: "'Leiding en Ondersteuning'!A1:E50",
     VerkennerRange: "Verkenners!A1:D100"
 };
